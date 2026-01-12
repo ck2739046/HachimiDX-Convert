87e88dfd89f56f065ab5995fdf211f9e0d52e5f7
simon <ck2739046@gmail.com>
2026-01-12 12:09:25 +1100

    [+] services: all services

---
+++ src/services/__init__.py
@@ -0,0 +1,10 @@
+from .all_services import AllServices
+# from .i18n_manage import I18nManage
+from .path_manage import PathManage
+from .settings_manage import SettingsManage
+
+__all__ = [
+    "AllServices",
+    "PathManage",
+    "SettingsManage",
+]
---
+++ src/services/all_services.py
@@ -0,0 +1,57 @@
+# 导入实例
+from core.schemas.op_result import OpResult, ok, err
+
+from .path_manage import PathManage
+from .settings_manage import SettingsManage
+from .i18n_manage import I18nManage
+import i18n
+
+
+class AllServices:
+
+    _is_initialized = False
+
+    @classmethod
+    def initialize_all(cls) -> OpResult[None]:
+        if cls._is_initialized:
+            return ok()
+        
+        print("Initializing all services...") # 此时i18n尚未初始化，只能英语
+
+
+        result = PathManage.init()
+        if result.is_ok:
+            print("PathManage initialization completed.")
+        else:
+            return err("Failed to initialize PathManage.", inner=result)
+
+        result = SettingsManage.init()
+        if result.is_ok:
+            print("SettingsManage initialization completed.")
+        else:
+            return err("Failed to initialize SettingsManager.", inner=result)
+        
+        result = I18nManage.init()
+        if result.is_ok:
+            print("I18nManage initialization completed.")
+        else:
+            return err("Failed to initialize I18nManage.", inner=result)
+        
+
+        print(i18n.t("all_services.notice_all_initialized"))
+        cls._is_initialized = True
+        return ok()
+
+
+
+    @classmethod
+    def shutdown_all(cls) -> None:
+        if not cls._is_initialized:
+            return
+        
+        print(i18n.t("all_services.notice_shutting_down_all"))
+
+        # 关闭顺序要反着来
+
+        print(i18n.t("all_services.notice_all_shutdown"))
+        cls._is_initialized = False
