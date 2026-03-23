a9a3f7b6714ac875759a5045e22c486563c04300
simon <49937999+ck2739046@users.noreply.github.com>
2026-03-24 00:21:43 +0800

    [+] build auto convert cmd: 为 detect 增加获取参数

--- src/core/build_auto_convert_cmd.py
+++ src/core/build_auto_convert_cmd.py
@@ -4,8 +4,9 @@ from src.services.path_manage import PathManage
 from .schemas.auto_convert_model import AutoConvertModel
 from .schemas.auto_convert_config import AutoConvertConfig_Definitions as AC_Defs
 from .schemas.auto_convert_config import AutoConvertConfig_Definition
-
 from .schemas.op_result import OpResult, ok, err
+from src.services.settings_manage import SettingsManage
+from .schemas.settings_config import SettingsConfig_Definitions as SC_Defs
 
 
 def build_auto_convert_cmd(data: AutoConvertModel) -> OpResult[list[str]]:
@@ -38,6 +39,19 @@ def build_auto_convert_cmd(data: AutoConvertModel) -> OpResult[list[str]]:
         # add detect args
         if data.is_detect_enabled:
             cmd.extend(_parse_fields(data, "detect"))
+
+            # 增加一些额外的参数
+            res = _get_detect_args_from_settings()
+            if not res.is_ok:
+                return err("Failed to get detect args from settings", inner=res)
+            cmd.extend(res.value)
+            # 增加模型路径
+            res = _get_detect_model_paths()
+            if not res.is_ok:
+                return err("Failed to get model paths", inner=res)
+            cmd.extend(res.value)
+
+
 
         # add analyze args
         if data.is_analyze_enabled:
@@ -81,3 +95,61 @@ def _parse_fields(data: AutoConvertModel, group: str) -> list[str]:
         args.append(arg_value)
 
     return args
+
+
+
+
+
+
+def _get_detect_args_from_settings() -> OpResult[list]:
+                
+    cmd = []
+
+    res = SettingsManage.get(SC_Defs.inference_device.key)
+    if not res.is_ok:
+        return err(f"Failed to get {SC_Defs.inference_device.key} from settings", inner=res)
+    cmd.append(f"--{SC_Defs.inference_device.key}")
+    cmd.append(str(res.value))
+
+    res = SettingsManage.get(SC_Defs.predict_batch_size_detect_obb.key)
+    if not res.is_ok:
+        return err(f"Failed to get {SC_Defs.predict_batch_size_detect_obb.key} from settings", inner=res)
+    cmd.append(f"--{SC_Defs.predict_batch_size_detect_obb.key}")
+    cmd.append(str(res.value))
+
+    res = SettingsManage.get(SC_Defs.predict_batch_size_classify.key)
+    if not res.is_ok:
+        return err(f"Failed to get {SC_Defs.predict_batch_size_classify.key} from settings", inner=res)
+    cmd.append(f"--{SC_Defs.predict_batch_size_classify.key}")
+    cmd.append(str(res.value))
+
+    return ok(cmd)
+
+
+
+
+
+def _get_detect_model_paths() -> OpResult[list]:
+
+    cmd = []
+
+    res = SettingsManage.get(SC_Defs.model_backend.key)
+    if not res.is_ok:
+        return err(f"Failed to get {SC_Defs.model_backend.key} from settings", inner=res)
+    model_backend = res.value
+
+    res = SC_Defs.get_path_by_backend(model_backend)
+    if not res.is_ok:
+        return err(f"Failed to get model paths for backend: {model_backend}", inner=res)
+    paths = res.value
+
+    cmd.append(f"--detect_model_path")
+    cmd.append(str(paths[0]))
+    cmd.append(f"--obb_model_path")
+    cmd.append(str(paths[1]))
+    cmd.append(f"--cls_break_model_path")
+    cmd.append(str(paths[2]))
+    cmd.append(f"--cls_ex_model_path")
+    cmd.append(str(paths[3]))
+
+    return ok(cmd)
