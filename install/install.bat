cd04136ce7d9840ffab21ff72fd142e34fe7cca3
simon <49937999+ck2739046@users.noreply.github.com>
2026-05-09 01:15:48 +0800

    [+] install: add --no-warn-script-location to install.bat

--- install/install.bat
+++ install/install.bat
@@ -4,7 +4,7 @@ cd /d "%~dp0"
 @echo on
 
 :: 更新 pip
-"..\python\python.exe" -m pip install --upgrade pip
+"..\python\python.exe" -m pip install --upgrade pip --no-warn-script-location
 
 ::更新 wheel
 "..\python\python.exe" -m pip install wheel --no-warn-script-location
