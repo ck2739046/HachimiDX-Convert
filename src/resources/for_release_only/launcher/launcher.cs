161b502ab64d0f1eb4f95f92965e0cc4d3e3019b
simon <49937999+ck2739046@users.noreply.github.com>
2026-05-17 21:32:43 +0800

    [*] launcher: 修复 cmd 最小化出现神秘悬浮标题栏 bug
    使用 winexe + allocConsole 代替 win 默认 cmd

--- src/resources/for_release_only/launcher/launcher.cs
+++ src/resources/for_release_only/launcher/launcher.cs
@@ -7,39 +7,58 @@ class Program
     [DllImport("kernel32.dll")]
     private static extern IntPtr GetConsoleWindow();
 
+    [DllImport("kernel32.dll")]
+    private static extern bool AllocConsole();
+
+    [DllImport("kernel32.dll")]
+    private static extern bool FreeConsole();
+
     [DllImport("user32.dll")]
     private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
-    private enum ConsoleState
-    {
-        Restore = 9,
-        Minimize = 6
-    }
 
-    private static void SetConsoleState(ConsoleState state)
+
+
+
+    // 控制 console 状态
+    private static readonly int SW_RESTORE = 9;
+    private static readonly int SW_SHOWMINNOACTIVE = 7;
+
+    private static void SetConsoleState(int state)
     {
         var handle = GetConsoleWindow();
         if (handle != IntPtr.Zero)
-            ShowWindow(handle, (int)state);
+            ShowWindow(handle, state);
     }
 
-    static void Main()
-    {
-        SetConsoleState(ConsoleState.Minimize);
 
-        var proc = Process.Start(new ProcessStartInfo
-        {
-            FileName = @".\python\python.exe",
-            Arguments = "-u main.py",
-            UseShellExecute = false
-        });
+
+
+
+    public static void Main()
+    {
+        AllocConsole();
+
+        SetConsoleState(SW_SHOWMINNOACTIVE);
+
+        var proc = Process.Start
+        (
+            new ProcessStartInfo
+            {
+                FileName = @".\python\python.exe",
+                Arguments = "-u main.py",
+                UseShellExecute = false
+            }
+        );
         proc?.WaitForExit();
 
         if (proc?.ExitCode != 0)
         {
-            SetConsoleState(ConsoleState.Restore);
+            SetConsoleState(SW_RESTORE);
             Console.WriteLine("\n按任意键继续...\nPress any key to continue...");
             Console.ReadKey(true);
         }
+
+        FreeConsole();
     }
 }
